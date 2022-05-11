d4a40d8b1ef8a560a728dc9352b59ce201a2b338
sannykumar1711 <sanny.kumar@oodles.io>
2022-05-11 16:41:17 +0530

    add swagger.json

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
 const express = require("express")
 const app = express()
 const errorMiddleware = require("./middleware/error")
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
-
+var options = {
+    customCss: '.swagger-ui .topbar { display: none }'
+};
 //Route imports
 const product = require('./routes/productRoute')
 app.use("/api/v1",product)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+
 
 //middleware for error
 app.use(errorMiddleware)
