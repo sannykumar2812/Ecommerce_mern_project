06dd90b88a107871aa40cd2844e7017c3e86f999
sannykumar1711 <sanny.kumar@oodles.io>
2022-05-12 00:43:36 +0530

    corrent create product api

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-
-const connectDatabase = ()=>{
-    // connction and after port db name Ecommerce 127.0.0.1
-mongoose.connect(process.env.DB_URI,{useCreateIndex:true,useUnifiedTopology:true,
-    useNewUrlParser:true}).then(()=>{
-        console.log(`mongodb connection with server successfully..!!`)
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
     })
-}
+    .then((data) => {
+      console.log(`Mongodb connected with server: ${data.connection.host}`);
+    });
+};
 
-module.exports=connectDatabase;
+module.exports = connectDatabase;
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,27 +1,41 @@
 const express = require("express");
-const router = express.Router()
-
 const {
-    getAllProducts,
-    createProduct,
-    updateProduct,
-    deleteProduct,
-    getProductDetails
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductDetails,  
+  createProductReview,
+  getProductReviews,
+  deleteReview,
+  getAdminProducts,
 } = require("../controllers/productController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
-router.route('/products').get(getAllProducts)
+const router = express.Router();
 
-router.route('/product/new').post(createProduct)
+router.route("/products").get(getAllProducts);
 
-router.route('/product/:id').put(updateProduct)
+router
+  .route("/admin/products")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAdminProducts);
 
-router.route('/product/:id').delete(deleteProduct)
+router
+  .route("/admin/product/new")
+  .post(createProduct);
 
-router.route('/product/:id').get(getProductDetails)
+router
+  .route("/admin/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 
+router.route("/product/:id").get(getProductDetails);
 
+router.route("/review").put(isAuthenticatedUser, createProductReview);
 
-
-
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 
 module.exports = router;
